8eedf1d4767e1fce5d2c055b97459990d2f04b3d
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2025-04-28 21:11:00 -0300

    Adding CSS file and component 'Details'

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Details } from './pages/Details';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Details />
   </StrictMode>,
 )
