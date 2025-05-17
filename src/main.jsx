2d99f7cd9d5f28dd011b9423c6b1bb73e14a5243
devLarissaRodrigues <larissa.rodrigues@ccc.ufcg.edu.br>
2025-05-17 20:25:18 -0300

    Insertion of components, css and fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Details } from './pages/Details';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyles from './styles/global';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,15 @@
-import "./styles.css";
+import { Container } from "./styles";
+import { Button } from "../../components/Button";
 
 export function Details(){
-  
   return(
-    <h1>Hello World</h1>
+    <Container>
+      <h1>Hello World</h1>
+      <span>Larissa Rodrigues</span>
+      <Button title="Login" loading={true}/>
+      <Button title="Entrar"/>
+      <Button title="Voltar"/>
+    </Container>
   )
 }
 
